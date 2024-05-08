d07974200b05005b9f9006344debc2738e941fb3
V <toriatovawebdev@gmail.com>
2024-05-08 14:02:13 +0400

    Added HeaderBox component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
 
 const Home = () => {
+    const loggedIn = {firstName: 'John', lastName: 'Doe'}
     return (
-        <div>Home</div>
+        <section className="home">
+            <div className="home-content">
+                <header className="home-header">
+                   <HeaderBox
+                   type="greeting"
+                   title="Welcome"
+                   user={loggedIn?.firstName || 'Guest'}
+                   subtext="Access and manage your account and transactions efficiently."
+                   />
+                </header>
+            </div>
+        </section>
     )
 }
 export default Home
